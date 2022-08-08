9dc9c61b1a3c4962a56533d18bd1195d02aa155f
rPhase <25046850+rPhase@users.noreply.github.com>
2022-08-08 14:48:36 -0700

    create basic users ui with classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
