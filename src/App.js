e9a7f75fac955d2aca81e26ff11f13ebcdc7a793
rPhase <25046850+rPhase@users.noreply.github.com>
2022-08-09 11:22:57 -0700

    Add search and alert

--- src/App.js
+++ src/App.js
@@ -3,28 +3,67 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
+import { Alert } from './components/layout/Alert';
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users', {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    this.setState({ users: res.data, loading: false });
-  }
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get('https://api.github.com/users', {
+  //     headers: {
+  //       Accept: 'application/vnd.github.v3+json',
+  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  //     },
+  //   });
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true, alert: null });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`,
+      {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    // setTimeout(() => {
+    //   this.setState({ alert: null });
+    // }, 5000);
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0}
+            setAlert={this.setAlert}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
